885074ce06814e493cd281f2bda56118c5ddc7d2
Kostadin Zamanov <kzamanov@shutterfly.com>
2022-06-04 11:20:58 +0300

    Ignore files

--- Hackaton/Hackaton/RecipiesList/ListCell.swift
+++ Hackaton/Hackaton/RecipiesList/ListCell.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+import UIKit
+
+class ListCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var prepTimeLabel: UILabel!
+    
+}
