128271c8b0a8329a584add48cdf22b56daddabf5
Ivaylo Kalaydzhiev <ivaylo.kalaydzhiev@scalefocus.com>
2022-06-04 11:11:55 +0300

    Added detail recipe files

---
+++ Hackaton/Hackaton/DetailRecipeView/IndgredientCell/IngredientTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  IngredientTableViewCell.swift
+//  Hackaton
+//
+//  Created by Ivaylo Kalaydzhiev on 4.06.22.
+//
+
+import UIKit
+
+class IngredientTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
